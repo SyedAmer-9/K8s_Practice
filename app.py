098a799668c8a55c126527d58f732804de7475af
amxr <cosmiclover2003.com>
2025-11-08 02:56:18 +0530

    added style and html and app

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def home():
-    message =""
+    message = ""
     if request.method =="POST":
         name = request.form.get("name")
         message = f"Hello {name},Welcome to the Kubernetes Test applicaiton"
-        return render_template("indes.html",message=message)
+    return render_template("index.html",message=message)
     
 if __name__== "__main__":
     app.run(host="0.0.0.0",port=5001)
